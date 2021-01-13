88c69c7b2e03ccf531745f0f8873271d0245cae3
raj <raj@raj-workspace>
2021-01-13 17:25:18 +0530

    this is branch 1 commit

--- src/main/java/com/Interface/pro/InterfaceApplication.java
+++ src/main/java/com/Interface/pro/InterfaceApplication.java
@@ -1,5 +1,6 @@
 package com.Interface.pro;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/Interface/pro/restController/TestClass.java
@@ -0,0 +1,6 @@
+package com.Interface.pro.restController;
+
+public class TestClass {
+	
+
+}
